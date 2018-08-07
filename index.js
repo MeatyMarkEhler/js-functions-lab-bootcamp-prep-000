70c99945661fd9ed18b327f6339763a66d35507f
Mark Ehler <ehlerm42@gmail.com>
2018-08-07 17:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+ // write your code below! 
+var name = "Mom"
+
+function happyHolidays(){
+  return "Happy holidays!"}
+  
+function happyHolidaysTo(name){
+  console.log("Happy holidays, ${name}!")
+}
+
+function HappyHolidayTo(name, holiday){
+  return "Happy ${holiday}, ${name}"
+}
+
+function holidayCountdown(days, holidayoliday){
+  return "It's ${days} days until ${holiday}!"
+}
